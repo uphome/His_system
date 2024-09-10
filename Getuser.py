6396e7c2524084c0b5497b4656531b9e3b8e8102
uphome <1822358119@qq.com>
2024-09-11 00:37:46 +0800

    完成不同种类用户登录

--- Getuser.py
+++ Getuser.py
@@ -3,7 +3,7 @@ import pyodbc
 def Getuser():
     conn_str = (
         r'DRIVER={ODBC Driver 17 for SQL Server};'
-        r'SERVER=localhost;'  # 或者只是服务器名，如果未使用命名实例  
+        r'SERVER=localhost;'  
         r'DATABASE=His_info;'
         r'Trusted_Connection=yes;'
     )
@@ -14,7 +14,7 @@ def Getuser():
 
     # 创建一个Cursor对象并执行SQL查询
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM dbo.login_info')
+    cursor.execute('SELECT * FROM dbo.loginuser')
 
     # 获取所有记录
     rows = cursor.fetchall()
@@ -22,11 +22,35 @@ def Getuser():
     # 关闭Cursor和连接
     cursor.close()
     conn.close()
+    # 数据预处理
     user_dic = {}
+    login_dic = {};
+
+    # 1:收费人员 2:医生 3:检验科室人员 4:药房人员 5:系统超级用户
     for row in rows:
-        user_dic[row[0].strip()] = row[1].strip()
-    return user_dic;
+        if (row[2].strip() == '1'):
+            user_dic[row[0].strip()] = row[1].strip()
+            login_dic['1'] = user_dic
+            user_dic={}
+        if (row[2].strip() == '2'):
+            user_dic[row[0].strip()] = row[1].strip()
+            login_dic['2'] = user_dic
+            user_dic={}
+        if (row[2].strip() == '3'):
+            user_dic[row[0].strip()] = row[1].strip()
+            login_dic['3'] = user_dic
+            user_dic={}
+        if (row[2].strip() == '4'):
+            user_dic[row[0].strip()] = row[1].strip()
+            login_dic['4'] = user_dic
+            user_dic={}
+        if (row[2].strip() == '5'):
+            user_dic[row[0].strip()] = row[1].strip()
+            login_dic['5'] = user_dic
+            user_dic={}
 
 
+    return login_dic;
 
+print(Getuser())
 
--- login.py
+++ login.py
@@ -14,19 +14,24 @@ app.config['SECRET_KEY'] = secret_key
 def login():
     if request.method == 'POST':
         Uer = Getuser.Getuser();
-        #print('数据库数据为：')
-        #print(Uer)
+        print(Uer)
         username = request.form['username']
         password = request.form['password']
-        index=1;
-        for key in Uer.keys():
-            if key == username and Uer[username] == password:
-                # 登录成功，这里只是简单重定向到根URL，实际应用中可能需要设置会话等
-                index = 0;
-                return redirect(url_for('success'))
-        if index:
-                flash("账户或密码错误 请重新输入")
+        Kind = request.form['Kind']
 
+        cort=1;
+
+        for index in Uer.keys():
+            print(index)
+            if Kind ==index :
+                for index1 in Uer[index].keys():
+                    print(index1)
+                    if Uer[index][index1] == password   :
+                        cort = 0;
+                        return redirect(url_for('success'))
+        if cort:
+                flash("账户或密码错误 请重新输入")
+#TODO 报错信息 会一直显示不会消失BUG
     return render_template('login.html')
 
 
